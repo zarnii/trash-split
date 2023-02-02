e48c2954da02d1f706c5db8536457826887fa0d5
zarnii <65334759+zarnii@users.noreply.github.com>
2023-02-02 14:51:09 +0300

    Add files via upload

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+void split(string str, vector <string> &array);
+
+int main() {
+	vector <string> array;
+	split("It's split in C++", array);
+	for (int i = 0; i < array.size(); i++) {
+		cout << array[i] << endl;
+	}
+
+}
+
+void split(string str, vector <string> &array) {
+	//получаем указатель на первый элемент строки
+	char *pstr = &str[0];
+
+	//создаем пустую строку в которую будем добовлять слова
+	string word;
+
+	//идем до конца строки(нулевого символа)
+	while (*pstr != '\0') {
+		if (*pstr == ' ') {
+			++pstr;
+			array.push_back(word);
+			word.clear();
+		}
+		else {
+			word.push_back(*pstr);
+			++pstr;
+		}
+	}
+	array.push_back(word);
+}
